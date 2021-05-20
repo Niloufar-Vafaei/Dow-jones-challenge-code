f32dfccd4beb29764dc1afcee3ff3d1781b43535
Niloufar-Vafaei <Niloufar.vafaeie@gmail.com>
2021-05-20 19:20:44 -0400

    add

--- src/main/java/com/rbc/imp/stockmarkets/util/CsvFileReader.java
+++ src/main/java/com/rbc/imp/stockmarkets/util/CsvFileReader.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.rbc.imp.stockmarkets.mappers.IMapper;
 
+import com.rbc.imp.stockmarkets.models.DowJonesIndex;
+import com.rbc.imp.stockmarkets.models.DowJonesIndexDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
